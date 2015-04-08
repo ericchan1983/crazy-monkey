c721f267f170158283e15e9b33ac5f3a2a36664f
eric.chan <chenyijin1983@gmail.com>
2015-04-08 11:27:23 +0800

    redirect the output of the player to /dev/null

--- scripts/genymotion_start.sh
+++ scripts/genymotion_start.sh
@@ -4,4 +4,4 @@ vm_name=$1
 
 vboxmanage snapshot $vm_name restore "factory"
 
-$CRAZY_MONKEY_HOME/../genymotion/player --vm-name $vm_name &
+$CRAZY_MONKEY_HOME/../genymotion/player --vm-name $vm_name & > /dev/null 2>&1
