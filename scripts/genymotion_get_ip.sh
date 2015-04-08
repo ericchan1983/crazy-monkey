c7d81722a814cc39548b1c6cdb4b8b541fe35be6
Alfred Shi <memoriedsea@gmail.com>
2015-04-09 03:21:14 +0800

    Change the get ip method

--- scripts/genymotion_get_ip.sh
+++ scripts/genymotion_get_ip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 vm_name=$1
-sleep 15
+sleep 30
 # MAC=`VBoxManage showvminfo ${vm_name} | grep MAC | grep vboxnet0 | awk -F ":" '{print $3}' | cut -c 2-13`
 # MAC=`echo ${MAC} | awk  'BEGIN{FS=""}{print $1$2":"$3$4":"$5$6":"$7$8":"$9$10":"$11$12}' | tr '[:upper:]' '[:lower:]'`
 # MAC=`echo $MAC | sed -e 's/\([0-9A-Fa-f]\{2\}\)/\1:/g' -e 's/\(.*\):$/\1/' | tr '[:upper:]' '[:lower:]'`
@@ -8,21 +8,22 @@ sleep 15
 # IP=`arp -a | grep -i ${MAC} | grep -E -o  "([0-9]{1,3}[\.]){3}[0-9]{1,3}"`
 # IP=`VBoxManage guestproperty get "${vm_name}" "/VirtualBox/GuestInfo/Net/0/V4/IP"`
 
-for i in {1..5}
+$ANDROID_SDK_HOME/platform-tools/adb start-server &
+$ANDROID_SDK_HOME/platform-tools/adb start-server &
+
+for i in {1..10}
 do 
 	IP=`VBoxManage guestproperty get "${vm_name}" "androvm_ip_management" | awk '{print $2}'`
 	if [ -n "$IP" ] && [ "$IP" != "value" ]
 	then
-		ping_test=`ping ${IP} -c 3 | grep rtt | sed 's/rtt //g'`
-		if [ -n "$ping_test" ]
+		ping_test=`${ANDROID_SDK_HOME}/platform-tools/adb -s ${IP}:5555 shell echo READY`
+		is_success=`echo "${ping_test}" | grep READY`
+		if [ -n "$is_success" ]
 		then
 			echo $IP:5555 > $CRAZY_MONKEY_HOME/userdata/genymotion_$vm_name.ini
 			echo "Success"
 			break
-		else
-			sleep 3s
 		fi
-	else
-		sleep 3
 	fi
+	sleep 10
 done
