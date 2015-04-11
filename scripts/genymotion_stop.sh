6d8214bcdaec5ecc226cf978696bdb5d284088c9
Alfred Shi <memoriedsea@gmail.com>
2015-04-12 00:39:56 +0800

    Adjust the param for the genymotion server

--- scripts/genymotion_stop.sh
+++ scripts/genymotion_stop.sh
@@ -4,12 +4,13 @@ vm_name=$1
 ps aux | grep "genymotion/player --vm-name ${vm_name}" | grep -v grep | awk '{print $2}' | xargs -rt kill -9
 
 IP=`VBoxManage guestproperty get "${vm_name}" "androvm_ip_management" | awk '{print $2}'`
+ps aux | grep "android-sdk-linux/platform-tools/adb -s ${IP}:5555" | grep -v grep | awk '{print $2}' | xargs -rt kill -9
 $ANDROID_SDK_HOME/platform-tools/adb disconnect $IP:5555
 
 # VBoxManage controlvm $vm_name acpipowerbutton
 # VBoxManage controlvm $vm_name keyboardputscancode 1c
 # ps aux | grep "virtualbox/VBoxHeadless --comment ${vm_name}" | grep -v grep | awk '{print $2}' | xargs -rt kill -9
 
-VBoxManage controlvm $vm_name poweroff soft && VBoxManage snapshot $vm_name restore "factory" && echo "Success"
+VBoxManage controlvm $vm_name poweroff soft && echo "Success"
 
 exit 0
