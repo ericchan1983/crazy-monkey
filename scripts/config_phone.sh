cf278dd309fc7c01143bc518d823d6ebcf06d208
Alfred Shi <memoriedsea@gmail.com>
2015-04-12 19:13:57 +0800

    Add the timeout for config phone scripts

--- scripts/config_phone.sh
+++ scripts/config_phone.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 $ANDROID_SDK_HOME/platform-tools/adb -s $1 push $CRAZY_MONKEY_HOME/userdata/xposeDevice.txt /mnt/sdcard/.system
-timeout 240 $ANDROID_SDK_HOME/platform-tools/adb -s $1 shell am instrument -w com.crazymonkey.test009/android.test.InstrumentationTestRunner
+sleep 5
+timeout 90 $ANDROID_SDK_HOME/platform-tools/adb -s $1 shell am instrument -w com.crazymonkey.test009/android.test.InstrumentationTestRunner
