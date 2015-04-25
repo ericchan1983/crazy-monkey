47528486d51fa80683d95e8ebe76ed4772ee36cc
Alfred Shi <memoriedsea@gmail.com>
2015-04-26 02:06:00 +0800

    Add the feature not to restore the factory

--- scripts/genymotion_start.sh
+++ scripts/genymotion_start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 vm_name=$1
-
-VBoxManage snapshot $vm_name restore "factory"
+is_restore=$2
+if [ -n "$is_restore" ] && [ "$is_restore" != "false" ]; then
+	VBoxManage snapshot $vm_name restore "factory_2"
+fi
 
 # screen VBoxManage -startvm $vm_name
 nohup $CRAZY_MONKEY_HOME/../genymotion/player --vm-name $vm_name &
