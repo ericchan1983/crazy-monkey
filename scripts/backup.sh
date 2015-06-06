88db4e5b85738f39b7a948c2883539a6d4752ee9
eric.chan <chenyijin1983@gmail.com>
2015-06-06 16:58:06 +0800

    Fix the issue for the backup and restore scripts files, add the space for the if expression

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,19 +1,23 @@
 #!/system/bin/sh
 package=$1
 
-if [ ! -d "/mnt/sdcard/backup"]; then
+if [ ! -d "/mnt/sdcard/backup" ]; then
 	mkdir /mnt/sdcard/backup
 fi 
 
-if [ ! -d "/mnt/sdcard/backup/userdata"]; then
+if [ ! -d "/mnt/sdcard/backup/userdata" ]; then
 	mkdir /mnt/sdcard/backup/userdata
 fi 
 
-if [ ! -d "/mnt/sdcard/backup/shell"]; then
+if [ ! -d "/mnt/sdcard/backup/shell" ]; then
 	mkdir /mnt/sdcard/backup/shell
 fi 
 
+echo "copy the userdata from the apk folder"
 cp -r /data/data/$package/* /mnt/sdcard/backup/userdata/
+echo "remove the cache file"
 rm -r /mnt/sdcard/backup/userdata/cache/
+echo "remove the lib file"
 rm -r /mnt/sdcard/backup/userdata/lib/
+echo "compress the userdata"
 tar czvf /mnt/sdcard/backup/userdata.tar.gz /mnt/sdcard/backup/userdata/*
--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,22 +1,31 @@
 #!/system/bin/sh
 package=$1
 
-if [ ! -d "/mnt/sdcard/backup"]; then
+if [ ! -d "/mnt/sdcard/backup" ]; then
 	mkdir /mnt/sdcard/backup
 fi 
 
-if [ ! -d "/mnt/sdcard/backup/userdata"]; then
+if [ ! -d "/mnt/sdcard/backup/userdata" ]; then
 	mkdir /mnt/sdcard/backup/userdata
 fi 
 
-if [ ! -d "/mnt/sdcard/backup/shell"]; then
+if [ ! -d "/mnt/sdcard/backup/shell" ]; then
 	mkdir /mnt/sdcard/backup/shell
 fi 
 
+if [ -d "/mnt/sdcard/backup/shell/mnt" ]; then
+	rm -r /mnt/sdcard/backup/shell/mnt
+fi 
+
+echo "move the userdata into backup folder"
 mv /mnt/sdcard/userdata.tar.gz /mnt/sdcard/backup/userdata.tar.gz
-rm -r /mnt/sdcard/backup/shell/mnt
+echo "move all the files from the userdata"
 rm -r /mnt/sdcard/backup/userdata/*
+echo "exact the userdata"
 tar xzvf /mnt/sdcard/backup/userdata.tar.gz -C /mnt/sdcard/backup/shell/
+echo "cpoy the userdata to backup"
 cp -r /mnt/sdcard/backup/shell/mnt/sdcard/backup/userdata/*  /mnt/sdcard/backup/userdata/
+echo "copy the userdata into apk folder"
 cp -r /mnt/sdcard/backup/userdata/* /data/data/$package/
+echo "add the permission"
 chmod -R 777 /data/data/$package/*
