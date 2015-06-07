8fdf52ad8e3ece004a5eb90c78c58b7347df72c8
eric.chan <chenyijin1983@gmail.com>
2015-06-07 20:44:13 +0800

    Restart the crazy monkey instead of adb server

--- check_adb.sh
+++ check_adb.sh
@@ -10,14 +10,14 @@ while true
 do
 	adb devices && result=1 || result=0
 	if [ "$result" = "0" ]; then
-		#echo "Restarting Crazy Monkey since Adb crashed..."
-		#cd $CRAZY_MONKEY_HOME
-		#nohup ./boot.sh
-		#exit 0	
-		echo "Restarting the adb server"
-                pgrep adb | xargs -rt kill -9
-                adb start-server
-                adb devices
+		echo "Restarting Crazy Monkey since Adb crashed..."
+		cd $CRAZY_MONKEY_HOME
+		nohup ./boot.sh
+		break
+		#echo "Restarting the adb server"
+        #pgrep adb | xargs -rt kill -9
+        #adb start-server
+        #adb devices
 	fi
 	echo "Sleep 600s"
 	sleep 600
